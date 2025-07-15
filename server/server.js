1eb1cb745ba66c70dd31cd69176dac62640a3c2d
rukundowilson <rukundowilson5@gmail.com>
2025-07-15 17:15:01 +0200

    database config complete

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import express, { json } from "express";
+import db from "./src/config/db.js";
 
 const port = 8081
 const app = express()
 app.use(json())
 
-app.get("/",(req,res)=>{
+app.get("/",async(req,res)=>{
+    const resData = await db.query("SELECT 1+1 AS solution");
+    console.log("db test",resData)
     res.status(200).json({
         message : "hello world"
     })
